e4898bcd17dc5ce4b15464978d3dd47afac747c1
JacobRays <140740318+JacobRays@users.noreply.github.com>
2025-09-15 15:08:30 +0200

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import '../pages/home/home_dashboard_page.dart';
 import '../pages/merchants/merchant_dashboard_page.dart';
 import '../pages/transactions/transaction_preview.dart';
-import '../pages/admin/admin_dashboard.dart';
+import '../pages/admin/admin_dashboard_page.dart';
 import '../pages/wallet/wallet_page.dart';
 import '../pages/wallet/transaction_history_page.dart';
 import '../pages/wallet/recharge_user_page.dart';
@@ -13,18 +13,23 @@ import '../pages/wallet/withdraw_user_page.dart';
 class AppRoutes {
   static const String initial = '/';
   static const String home = '/home';
-  // ... other route names
+  static const String merchantDashboard = '/merchant-dashboard';
+  static const String transactionPreview = '/transaction-preview';
+  static const String adminDashboard = '/admin-dashboard';
+  static const String wallet = '/wallet';
+  static const String walletHistory = '/wallet/history';
+  static const String walletRecharge = '/wallet/recharge';
+  static const String walletWithdraw = '/wallet/withdraw';
 
   static Map<String, WidgetBuilder> routes = {
     initial: (context) => HomeDashboardPage(),
     home: (context) => HomeDashboardPage(),
-    '/merchant-dashboard': (context) => MerchantDashboard(),
-    '/transaction-preview': (context) => TransactionPreview(),
-    '/admin-dashboard': (context) => AdminDashboard(),
-    '/wallet': (context) => WalletPage(),
-    '/wallet/history': (context) => TransactionHistoryPage(),
-    '/wallet/recharge': (context) => RechargeUserPage(),
-    '/wallet/withdraw': (context) => WithdrawUserPage(),
+    merchantDashboard: (context) => MerchantDashboard(),
+    transactionPreview: (context) => TransactionPreview(),
+    adminDashboard: (context) => AdminDashboard(),
+    wallet: (context) => WalletPage(),
+    walletHistory: (context) => TransactionHistoryPage(),
+    walletRecharge: (context) => RechargeUserPage(),
+    walletWithdraw: (context) => WithdrawUserPage(),
   };
 }
-
