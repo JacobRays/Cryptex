9d2a1ac1e0f1244c835e9be2ee6b4cb278f8d83f
JacobRays <140740318+JacobRays@users.noreply.github.com>
2025-09-18 21:47:00 +0200

    Update neon.dart

--- lib/widgets/neon.dart
+++ lib/widgets/neon.dart
@@ -1,36 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:cryptex_malawi/theme/app_colors.dart';
-
-class NeonText extends StatelessWidget {
-  final String text;
-  final double fontSize;
-  final Color color;
-  final FontWeight weight;
-
-  const NeonText({
-    Key? key,
-    required this.text,
-    this.fontSize = 20,
-    this.color = AppColors.neon,
-    this.weight = FontWeight.w600,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(
-        fontSize: fontSize,
-        fontWeight: weight,
-        color: color,
-        shadows: [
-          Shadow(
-            blurRadius: 12,
-            color: color.withOpacity(0.6),
-            offset: const Offset(0, 0),
-          ),
-        ],
-      ),
-    );
-  }
-}
+// lib/widgets/neon.dart
+export 'neon_text.dart';
+export 'neon_button.dart';
+export 'neon_card.dart';
