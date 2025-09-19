c3cb0d0aa7f9d028a4d38bc16f7d6dd719cdbe87
JacobRays <140740318+JacobRays@users.noreply.github.com>
2025-09-19 15:10:14 +0200

    Update wallet_page.dart

--- lib/pages/wallet/wallet_page.dart
+++ lib/pages/wallet/wallet_page.dart
@@ -4,10 +4,16 @@ import '../../theme.dart';
 import '../../widgets/neon.dart';
 import '../../widgets/app_scaffold.dart';
 import 'wallet_overview.dart';
+import 'buy_usdt.dart';
+import 'sell_usdt.dart';
+import 'transaction_history.dart';
+import 'recharge.dart';
+import 'withdraw.dart';
+import 'manual_bank_withdrawal.dart';
+import 'transaction_preview_page.dart';
 
 class WalletPage extends StatefulWidget {
   WalletPage({Key? key}) : super(key: key);
-
   @override
   _WalletPageState createState() => _WalletPageState();
 }
@@ -25,6 +31,24 @@ class _WalletPageState extends State<WalletPage> {
             case '/wallet/overview':
             default:
               builder = (BuildContext _) => const WalletOverview();
+              break;
+            case '/wallet/buy':
+              builder = (BuildContext _) => const BuyUSDTPage();
+              break;
+            case '/wallet/sell':
+              builder = (BuildContext _) => const SellUSDTPage();
+              break;
+            case '/wallet/history':
+              builder = (BuildContext _) => const TransactionHistoryPage();
+              break;
+            case '/wallet/recharge':
+              builder = (BuildContext _) => const RechargePage();
+              break;
+            case '/wallet/withdraw':
+              builder = (BuildContext _) => const WithdrawPage();
+              break;
+            case '/wallet/manual-bank':
+              builder = (BuildContext _) => const ManualBankWithdrawalPage();
               break;
           }
           return MaterialPageRoute(builder: builder, settings: settings);
