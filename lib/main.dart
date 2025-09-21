8ee02e0404137e3b6341bf6687e6f7ce0ac918c1
JacobRays <140740318+JacobRays@users.noreply.github.com>
2025-09-21 22:53:12 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cryptex_malawi/routes/app_routes.dart';
-import 'package:cryptex_malawi/theme/app_theme.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(CryptexApp());
 }
 
@@ -14,7 +10,10 @@ class CryptexApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Cryptex Malawi',
-      theme: AppTheme.darkTheme,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF00D4FF),
+        scaffoldBackgroundColor: Color(0xFF0A0E27),
+      ),
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.initial,
       routes: AppRoutes.routes,
