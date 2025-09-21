e90ef644d43a0136075a31cfeed01258f4e2c437
JacobRays <140740318+JacobRays@users.noreply.github.com>
2025-09-21 20:23:57 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
-import './routes/app_routes.dart';
-import './theme/app_theme.dart';
+import 'package:cryptex_malawi/routes/app_routes.dart';
+import 'package:cryptex_malawi/theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // ✅ Firebase initialization
-  runApp(const CryptexApp());
+  await Firebase.initializeApp();
+  runApp(CryptexApp());
 }
 
 class CryptexApp extends StatelessWidget {
-  const CryptexApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Cryptex Malawi',
+      theme: AppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.dark,
       initialRoute: AppRoutes.initial,
       routes: AppRoutes.routes,
-      builder: (context, child) {
-        return MediaQuery(
-          data: MediaQuery.of(context).copyWith(
-            textScaler: TextScaler.linear(1.0),
-          ),
-          child: child!,
-        );
-      },
     );
   }
 }
