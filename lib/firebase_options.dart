ededf341f8d51c3f98a5014dea11700a2848101c
JacobRays <140740318+JacobRays@users.noreply.github.com>
2025-09-22 11:56:12 +0200

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for iOS - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyAMN-J6P_U-sM3QP4MhU68zPeRz4skenHU',
+    appId: '1:355167642913:android:bfcd2b54fb7b8a7587ca60',
+    messagingSenderId: '355167642913',
+    projectId: 'cryptex-malawi',
+    storageBucket: 'cryptex-malawi.firebasestorage.app',
+  );
+}
